78aaaaa3a8f4df8eac76cfbc308391cb1045cb08
Yash Parakh <yash.parakh.jain.23@gmail.com>
2025-07-03 17:48:03 +0530

    Bring app to root

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hello, this is my personal website! This Site is Under Construction.
+    </div>
+  );
+}
